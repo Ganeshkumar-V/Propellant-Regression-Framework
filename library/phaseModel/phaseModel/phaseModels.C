e8376c516475b224e989fcdc2f260ec15b56febc
Ganeshkumar-V <ganeshkv@iitgn.ac.in>
2022-12-09 15:48:36 +0530

    Addition of AnisothermalParticlePhaseModel

---
+++ library/phaseModel/phaseModel/phaseModels.C
@@ -0,0 +1,71 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | www.openfoam.com
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+    Copyright (C) 2015-2018 OpenFOAM Foundation
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "addToRunTimeSelectionTable.H"
+
+#include "rhoThermo.H"
+#include "rhoReactionThermo.H"
+
+#include "CombustionModel.H"
+
+#include "phaseModel.H"
+#include "ThermoPhaseModel.H"
+#include "AnisothermalParticlePhaseModel.H"
+#include "PurePhaseModel.H"
+#include "InertPhaseModel.H"
+#include "MovingPhaseModel.H"
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+namespace Foam
+{
+    typedef
+        AnisothermalParticlePhaseModel
+        <
+            PurePhaseModel
+            <
+                InertPhaseModel
+                <
+                    MovingPhaseModel
+                    <
+                        ThermoPhaseModel<phaseModel, rhoThermo>
+                    >
+                >
+            >
+        >
+        pureParticlePhaseModel;
+
+    addNamedToRunTimeSelectionTable
+    (
+        phaseModel,
+        pureParticlePhaseModel,
+        phaseSystem,
+        pureParticlePhaseModel
+    );
+}
+
+// ************************************************************************* //
